5f11c4b430dd9ba6acfc8e588fd04af47652dba3
Rachel - Windows <rachenn@gmail.com>
2023-06-06 17:04:44 +0800

    Configuring for THG initial page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
+import logo from './image/THG_NoBG.png';
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} class="App-logo" alt="logo" />
+        <p>Make sure to follow us on our socials to keep up to date, or join our mailing list below!</p>
+        <FontAwesomeIcon icon="fa-brands fa-facebook-f" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeGhgolUg5iAJwQQTfGs0MHFQAhd_zn8AYddR_YOwvmq7QTUQ/viewform?embedded=true" width="640" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
